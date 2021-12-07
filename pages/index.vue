1a430de8ce54ac090f67ee1688b04c0a7258a862
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-12-07 17:13:22 +0530

    quick start waheguru ji.. got some fonts!

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-4">
+    <font-awesome-icon icon="home" />Waheguru 13 shukar hai! Dhan Guru Nanak Dev
+    Sahib ji!
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
