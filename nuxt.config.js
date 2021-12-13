e1bcbae20172d853ddd5fca77bae3feb55bf48e1
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-12-13 14:03:52 +0530

    add persistent vuex

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vuex-persist.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
